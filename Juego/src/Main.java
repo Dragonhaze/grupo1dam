fc8f276d65b3f62ed78956dab51f5fa0c979cd51
Dragonhaze <duncanporter@hotmail.es>
2017-04-18 13:30:04 +0200

    Final main

--- Juego/src/Main.java
+++ Juego/src/Main.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int userInput;
 		
+		System.out.println("Elige uno de los siguientes juegos");
+		System.out.println("Introduce 1 para un acertijo");
+		System.out.println("Introduce 2 para jugar al ahorcado");
+		System.out.println("Introduce 3 para jugar a pensar un número");
+		
+		Scanner input = new Scanner(System.in);
+		
+		userInput = input.nextInt();
+		
+		
+		if(userInput==1){
 		Acertijo juego1 = new Acertijo();
 		
 		System.out.println("Si 5 gatos cazan 5 ratones en 5 minutos, ¿Cuantos gatos cazaran 100 ratones en 100 minutos?");
 
 		juego1.añadirRespuesta(5);
+		}
+		
+		if(userInput==2){
+			Ahorcado test = new Ahorcado();
+			Ahorcado.main(args);
+		}
+		if(userInput==3){
+			AdivinarNum test2 = new AdivinarNum();
+			test2.empiezaJuego();
+		}
 	}
 
 }
