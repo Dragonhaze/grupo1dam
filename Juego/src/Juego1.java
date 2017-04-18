062788f0cc37538c91a3691268d1f80b1fc27f3e
maria_000 <maria_000@equipo-mouse>
2017-04-18 12:45:28 +0200

    condicion en setters

--- Juego/src/Juego1.java
+++ Juego/src/Juego1.java
@@ -5,41 +5,52 @@ public class Juego1 {
 	
 	//Complements
 	
-	private String mentira;
 	private String verdad;
+	private int respuesta;
 	
 	//Constructor
-	public Juego1(String mentira, String verdad) {
-		super();
-		this.mentira = mentira;
+	
+	public Juego1() {
+		
+	}
+
+	public Juego1( String verdad) {
+		
 		this.verdad = verdad;
 	}
 
 	//Getters and setters
-	
-	public String getMentira() {
-		return mentira;
-	}
-
-
-	public void setMentira(String mentira) {
-		this.mentira = mentira;
-	}
-
 
 	public String getVerdad() {
 		return verdad;
 	}
 
 
-	public void setVerdad(String verdad) {
+	public void setVerdad(String verdad,int respuesta) {
+			
 		this.verdad = verdad;
+		
 	}
 
+	public int getRespuesta() {
+		return respuesta;
+	}
+
+	public int setRespuesta(int respuesta) {
+		
+		if(respuesta==5){
+			this.respuesta = respuesta;
+		}else{
+		
+		}
+		
+		return respuesta;
+	
+	}
 
 	@Override
 	public String toString() {
-		return "Juego1 [mentira=" + mentira + ", verdad=" + verdad + "]";
+		return "Juego1 [verdad=" + verdad + "]";
 	}
 	
 }
