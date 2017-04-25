d7bb1604710cf4308e22539cc385dc420dbd5ec2
Dragonhaze <duncanporter@hotmail.es>
2017-04-25 12:55:38 +0200

    Comentarios y pequeñas optimizaciones al codigo de adivinanumeros

--- Juego/src/AdivinarNum.java
+++ Juego/src/AdivinarNum.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * Esta clase es un juego que adivinana un número que estas pensando
+ * @author Duncan Porter
+ */
+
 public class AdivinarNum {
-	int max;
+	/**
+	 * <h1>Variables</h1>
+	 * 
+	 *
+	 */
+	int max;	
 	int min;
 	int guess;
 	int userInput;
@@ -27,30 +37,27 @@ public class AdivinarNum {
 		this.guess = guess;
 	}
 	
-	//Contructores
-	
+	//Constructor vacio
 	public AdivinarNum(){
-		
-	}
-	
-	public AdivinarNum(int max, int min, int guess) {
-		super();
-		this.max = max;
-		this.min = min;
-		this.guess = guess;
+		empiezaJuego();
 	}
 	
 	
 	
-	
+	/*
+	 * Se inicia el juego y se inicializa las variables desadas.
+	 * 
+	 * @param max: Es el numero máximo en el que puede estar el número pensado por el usuario.
+	 * @param min: Es el numero minimo en el que puede estar el número pensado por el usuario.
+	 * @param guess: Es el numero en al que llegua el ordenador después del algoritmo.
+	 * @param userInput: El numero que introduce el usuario.
+	 */
 	public void empiezaJuego (){
-		this.max = 1000;
-		this.min = 1;
-		this.guess=500;
+		this.setMax(1000);
+		this.setMin(1);
+		this.setGuess(500);
 		
 		System.out.println("Piensa en un numero del "+min+" al "+max);
-		
-		
 		
 		System.out.println("Es el número mayor o menor que "+guess+"?");
 		System.out.println("Si es mayor introduce 2 o si es menor 1 y si es correcto introduce 3");
@@ -60,28 +67,34 @@ public class AdivinarNum {
 		
 		
 	}
-	
+	/**
+	 * Este metodo se repite cada vez que el usuario tiene contestar a la pregunta
+	 * si responde 1 , 2 o 3 el programa sigue y si no sigue preguntando.
+	 */
 	public void update(){
 		
 		Scanner input= new Scanner(System.in);
 		userInput = input.nextInt();
+		while(userInput!=1|userInput!=2|userInput!=3){
+			if(userInput==2){
+				min = guess;
+				sigueJuego();
+			}
+			
+			if(userInput==1){
+				max = guess;
+				sigueJuego();
+			}
 		
-		if(userInput==2){
-			min = guess;
-			sigueJuego();
-		}
-		
-		if(userInput==1){
-			max = guess;
-			sigueJuego();
-		}
-		
-		if(userInput==3){
-			System.out.println("Gane!!");
-			System.exit(0);
+			if(userInput==3){
+				System.out.println("Gane!!");
+				System.exit(0);
+			}
 		}
 	}
-	
+	/**
+	 * Este metodo sigue el juego despues de que el usuario responda a la pregunta 
+	 */
 	public void sigueJuego (){
 		
 		guess = (max + min)/2;
